58d02a25a2b35c4043f80e907197c63d60e79cc0
Jesen Tanadi <mail@jesentanadi.com>
2019-09-07 15:56:43 -0400

    completed checkpoint

--- models/task.model.js
+++ models/task.model.js
@@ -22,6 +22,54 @@ const Task = db.define('Task', {
   due: Sequelize.DATE
 });
 
+Task.clearCompleted = async function() {
+  await this.destroy({ where: { complete: true } })
+}
+
+Task.completeAll = async function() {
+  await this.update(
+    { complete: true },
+    { where: { complete: false },
+  })
+}
+
+Task.prototype.getTimeRemaining = function() {
+  const dueDate = this.get().due
+  return (!dueDate) ? Infinity : dueDate - Date.now()
+}
+
+Task.prototype.isOverdue = function() {
+  const dueDate = this.get().due
+  const completed = this.get().complete
+  const now = Date.now()
+
+  return (!completed && now > dueDate)
+}
+
+Task.prototype.addChild = async function(child) {
+  const newChild = await Task.create({ name: child.name, parentId: this.id})
+  let children = this.getDataValue("children")
+
+  if (!children) {
+    children = [];
+  }
+
+  children.push(newChild)
+  this.setDataValue("children", children)
+
+  return newChild
+}
+
+Task.prototype.getChildren = function() {
+  return this.get().children
+}
+
+Task.prototype.getSiblings = async function() {
+  const parentId = this.get().parentId
+  const siblings = await Task.findAll({ where: { parentId } })
+  return siblings.filter(child => child.id !== this.id)
+}
+
 
 
 Task.belongsTo(Task, {as: 'parent'});
